f587f2cfb8de2a57d1b7e53b27c5226c23940a54
Gatzmoz <gilangminecraft21@gmail.com>
2024-06-21 23:29:04 +0700

    changing font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from 'next/font/google';
+import { Figtree } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar';
 
-const inter = Inter({ subsets: ['latin'] });
+const figtree = Figtree({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Create Next App',
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className={figtree.className}>
 				<Navbar />
 				<main className='mx-auto flex-grow'>{children}</main>
 			</body>
