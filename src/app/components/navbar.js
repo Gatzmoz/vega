29996174881d7fecfa905de98048739edf77b2fa
Gatzmoz <gilangminecraft21@gmail.com>
2024-06-21 23:11:22 +0700

    initial commit

---
+++ src/app/components/navbar.js
@@ -0,0 +1,69 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+export default function Navbar() {
+	const route = usePathname();
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scroll = window.scrollY > 0;
+			setIsScrolled(scroll);
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	const navLinks = [
+		{
+			name: 'Artikel',
+			link: '/article',
+		},
+		{
+			name: 'Anggota',
+			link: '/anggota',
+		},
+		{
+			name: 'Kontak',
+			link: '/contact',
+		},
+	];
+
+	return (
+		<nav
+			className={`mx-auto w-screen bg-blue-500 px-20 py-4 flex items-center text-white fixed transition ease-in duration-150 ${
+				route === '/' ? (isScrolled ? '' : 'bg-transparent') : ''
+			}`}
+		>
+			<div className='navbar-brand z-20'>
+				<Link
+					href='/'
+					className={`uppercase text-xl font-extrabold ${
+						route === '/' ? (isScrolled ? '' : 'hidden') : ''
+					}`}
+				>
+					Vega
+				</Link>
+			</div>
+			<div className='nav-items ms-auto justify-evenly space-x-4 hidden md:block z-20'>
+				{navLinks.map(function (navLink) {
+					return (
+						<Link
+							key={navLink.link}
+							href={navLink.link}
+							className={`hover:text-blue-950 font-medium ${
+								route == navLink.link ? 'active' : ''
+							}`}
+						>
+							{navLink.name}
+						</Link>
+					);
+				})}
+			</div>
+		</nav>
+	);
+}
